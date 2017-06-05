1fb3e887b29cbe097319853a708cbe3393e49b5c
Ben Greenman <benjaminlgreenman@gmail.com>
2017-06-05 01:35:59 -0400

    [private] more cases for 'parse-type'

--- private/parse-type.rkt
+++ private/parse-type.rkt
@@ -16,25 +16,37 @@
   ;; TODO https://github.com/philnguyen/soft-contract/issues/82
   ;; syntax->datum
   (syntax-parse stx
-   #:literals (tr--> tr-Any tr-Natural tr-Integer tr-String tr-Listof tr-Vectorof tr-Boolean tr-HashTable)
+   #:literals (tr--> tr-Any tr-True tr-False tr-Real tr-Natural tr-Integer tr-String tr-Pairof tr-Listof tr-Vectorof tr-Boolean tr-HashTable)
    [(tr--> . arg*)
     (cons '-> (map syntax->type-rep (syntax-e #'arg*)))]
+   [(tr-U . t*)
+    (cons 'or/c (map syntax->type-rep (syntax-e #'t*)))]
    [(tr-Listof t)
     (list 'listof (syntax->type-rep #'t))]
    [(tr-Vectorof t)
     (list 'vectorof (syntax->type-rep #'t))]
    [(tr-HashTable k v)
     (list 'hash/c (syntax->type-rep #'k) (syntax->type-rep #'v))]
+   [(tr-Pairof a b)
+    (list 'cons/c (syntax->type-rep #'a) (syntax->type-rep #'b))]
    [tr-Natural
     'exact-nonnegative-integer?]
+   [tr-Real
+    'real?]
    [tr-Integer
     'integer?]
    [tr-String
     'string?]
    [tr-Boolean
     'boolean?]
+   [tr-True
+    '#t]
+   [tr-False
+    '#f]
    [tr-Any
     'any/c]
+   [x:id
+    (string->symbol (string-downcase (format "~a?" (syntax-e #'x))))]
    [_
     (raise-user-error 'syntax->type-rep "cannot parse type ~a" (syntax->datum stx))]))
 
